7aa01be02e892f9273f25ca7d4d12b739c139d5b
Pavnoji <pavnoji@amazon.com>
2017-04-02 03:25:26 +0530

    Changed database connection url

--- dbconnect.php
+++ dbconnect.php
@@ -1,22 +1,15 @@
 <?php
 
-	$server = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "airport";
-    $mysqli = new mysqli($server, $username, $password, $dbname);
+        $server = "us-cdbr-iron-east-03.cleardb.net";
+            $username = "b8362f7492b419";
+            $password = "b790c879";
+            $dbname = "ad_4b9db46d62be7e3";
+
+$mysqli = new mysqli($server, $username, $password, $dbname);
 
     if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') '
             . $mysqli->connect_error);
 	}
-// $con=mysqli_connect("localhost","root","","supercoders");
-// $check="SELECT * FROM userdet WHERE uticket = '1403'";
-// $rs = mysqli_query($con,$check);
-// $data = mysqli_fetch_array($rs, MYSQLI_NUM);
-// if($data[0] > 0) {
-//     echo json_encode($data);
-
-// }
 
 ?>
